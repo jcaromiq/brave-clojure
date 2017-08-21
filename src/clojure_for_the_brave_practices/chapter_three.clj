89e5c42559c88db30c5871037524874b1a4ca635
Joaquin Caro <me@joaquin-caro.es>
2017-08-21 16:32:03 +0200

    Refactor: group by chapters

--- src/clojure_for_the_brave_practices/chapter_three.clj
+++ src/clojure_for_the_brave_practices/chapter_three.clj
@@ -1,14 +1,12 @@
-(ns clojure-for-the-brave-practices.destructuring
-  (:gen-class))
+(ns clojure-for-the-brave-practices.chapter-three)
 
-
+;; ------- DESTRUCTURING -------
 ;; Return the first element of a collection
 (defn my-first
   [[first-thing]]
   first-thing)
 (my-first ["first item", "second item", "third item"])
 
-;;
 (defn chosser
   [[first-choice second-choice & unimportant-choices]]
   (println (str "Your first choice is: " first-choice))
@@ -38,3 +36,37 @@
   treasure-location)
 
 (receive-treasure-location {:lat 28.22 :lng 81.33})
+
+;; ------- FUNCTIONS -------
+(ns clojure-for-the-brave-practices.functions)
+(map (fn [name] (str "Hi, " name))
+     ["Darth Vader", "Mr. Magoo"])
+
+((fn [x] (* x 3)) 8)
+
+(def my-multiplier (fn [x] (* x 3)))
+(my-multiplier 12)
+
+(def my-divider #(/ % 3))
+(my-divider 3)
+
+(map #(str "Hi, " %)
+     ["Darth Vader", "Mr. Magoo"])
+
+(#(str %1 " and " %2) "cornbread" "butter beans")
+
+(#(identity %&) 1 "dos" :tres ["Darth Vader", "Mr. Magoo"])
+
+;; ------- RETURNING FUNCTIONS -------
+(ns clojure-for-the-brave-practices.returning-functions)
+
+(defn inc-maker
+  "Create a custom incrementor"
+  [inc-by]
+  #(+ % inc-by))
+
+(def inc3 (inc-maker 3))
+(def inc5 (inc-maker 5))
+
+(inc3 7)
+(inc5 10)
